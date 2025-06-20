8177517d1e3a3d070b70db9a972ccc9490f9e6e6
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2025-06-21 02:32:26 +0530

    fix: remove unused imports and clean Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,13 @@
-import React from 'react'
-import { useContext } from 'react';
-import NoteContext from '../context/notes/NoteContext';
+import React from 'react';
 import Note from './Notes'; 
 import AddNote from './AddNote';
 
-
-
 export default function Home(props) {
-  const {showAlert} = props;
+  const { showAlert } = props;
   return (
     <div>
-      <AddNote showAlert={showAlert}/>
-      
-      <Note showAlert={showAlert}/>
+      <AddNote showAlert={showAlert} />
+      <Note showAlert={showAlert} />
     </div>
-  )
+  );
 }
-
