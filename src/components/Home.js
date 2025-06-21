6e35d1cb143f3ac1e166082ce3980d145cbf8a36
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2025-06-21 12:20:42 +0530

    Fix: Home route redirect if not logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import Note from './Notes'; 
+import React, { useEffect } from 'react';
+import Note from './Notes';
 import AddNote from './AddNote';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home(props) {
+
+  const Navigate = useNavigate();
   const { showAlert } = props;
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      showAlert("Please login first", "warning");
+      Navigate('/login-signup');
+    }
+  }, [showAlert, Navigate]);
+
+
   return (
     <div>
       <AddNote showAlert={showAlert} />
